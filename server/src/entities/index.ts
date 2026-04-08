67daa897e865b38bb4a0e41bbdeb1568ecfcec8a
Rashmi <rashmi0505an@gmail.com>
2026-04-08 11:35:13 +0530

    adding core entity classes, also implementing interfaces for system

---
+++ server/src/entities/index.ts
@@ -0,0 +1,8 @@
+export { User } from "./user";
+export { Patient } from "./patient";
+export { Doctor } from "./doctor";
+export { Admin } from "./admin";
+export { Appointment } from "./appointment";
+export { WalkInAppointment } from "./walk_in_appointment";
+export { ScheduledAppointment } from "./scheduled_appointment";
+export { EmergencyAppointment } from "./emergency_appointment";
