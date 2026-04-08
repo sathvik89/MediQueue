269487851df42870263a5a68a63c89b2b9095a93
Rashmi <rashmi0505an@gmail.com>
2026-04-08 11:44:14 +0530

    reverting entity changes from main

--- server/src/entities/index.ts
+++
@@ -1,8 +0,0 @@
-export { User } from "./user";
-export { Patient } from "./patient";
-export { Doctor } from "./doctor";
-export { Admin } from "./admin";
-export { Appointment } from "./appointment";
-export { WalkInAppointment } from "./walk_in_appointment";
-export { ScheduledAppointment } from "./scheduled_appointment";
-export { EmergencyAppointment } from "./emergency_appointment";
